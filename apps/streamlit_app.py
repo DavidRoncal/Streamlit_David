b4a680a6bd1003bf8262ab555e31eab8b20413c8
DavidRoncal <71480596+DavidRoncal@users.noreply.github.com>
2022-07-24 22:29:46 -0500

    Update streamlit_app.py

--- apps/streamlit_app.py
+++ apps/streamlit_app.py
@@ -2,40 +2,42 @@ import streamlit as st
 from streamlit_image_comparison import image_comparison
 import cv2
 
-# set page config
-st.set_page_config(
-    page_title="James Webb Space Telescope vs Hubble Telescope Images",
-    layout="centered",
-)
 
-st.image(
-    "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/325/telescope_1f52d.png",
-    width=120,
-)
+def app():
+    # set page config
+    st.set_page_config(
+        page_title="James Webb Space Telescope vs Hubble Telescope Images",
+        layout="centered",
+    )
 
-st.header("J. Webb Space Telescope vs Hubble Telescope")
+    st.image(
+        "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/325/telescope_1f52d.png",
+        width=120,
+    )
 
-st.write("")
-st.write("A simple tool to compare Webb's new images to Hubble!")
-st.write("")
+    st.header("J. Webb Space Telescope vs Hubble Telescope")
 
-st.markdown("### Southern Nebula")
+    st.write("")
+    st.write("A simple tool to compare Webb's new images to Hubble!")
+    st.write("")
 
-# render image-comparison
-image_comparison(
-    img1="https://www.webbcompare.com/img/hubble/southern_nebula_700.jpg",
-    img2="https://www.webbcompare.com/img/webb/southern_nebula_700.jpg",
-    label1="Hubble",
-    label2="Webb",
-)
+    st.markdown("### Southern Nebula")
+
+    # render image-comparison
+    image_comparison(
+        img1="https://www.webbcompare.com/img/hubble/southern_nebula_700.jpg",
+        img2="https://www.webbcompare.com/img/webb/southern_nebula_700.jpg",
+        label1="Hubble",
+        label2="Webb",
+    )
 
 
-st.markdown("### Galaxy Cluster SMACS 0723")
+    st.markdown("### Galaxy Cluster SMACS 0723")
 
-# render image-comparison
-image_comparison(
-    img1="https://www.webbcompare.com/img/hubble/deep_field_700.jpg",
-    img2="https://www.webbcompare.com/img/webb/deep_field_700.jpg",
-    label1="Hubble",
-    label2="Webb",
-)
+    # render image-comparison
+    image_comparison(
+        img1="https://www.webbcompare.com/img/hubble/deep_field_700.jpg",
+        img2="https://www.webbcompare.com/img/webb/deep_field_700.jpg",
+        label1="Hubble",
+        label2="Webb",
+    )
