cb93a1eb15f2002f0e8256fad436b196c3a5bea3
DavidRoncal <71480596+DavidRoncal@users.noreply.github.com>
2022-07-24 22:13:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from multiapp import MultiApp
-from apps import home, basemaps, customize, datasets, opacity, nlcd_demo, Raura, Raura_dem
+from apps import home, basemaps, customize, datasets, opacity, nlcd_demo, Raura, Raura_dem, streamlit_app
 
 st.set_page_config(layout="wide")
 
@@ -17,6 +17,6 @@ apps.add_app("Search datasets", datasets.app)
 apps.add_app("NLCD Demo", nlcd_demo.app)
 apps.add_app("Imágen Satelial Raura", Raura.app)
 apps.add_app("Modelo de Elevación Digital", Raura_dem.app)
-
+apps.add_app("Comparación Imágenes", streamlit_app.app)
 # The main app
 apps.run()
